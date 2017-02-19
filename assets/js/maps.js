9604c3e6d2f42f79dd5fabe83ceaa45fbcb59575
David Lebrun <dave.lebr1@gmail.com>
2017-02-18 22:27:26 -0500

    fix marker bug

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -11,7 +11,7 @@ function initMap() {
 
 	initSearchBox();
 	map.addListener('idle', function() {
-		fetchSpotted();	
+		fetchSpotteds();	
   	});	
 }
 
@@ -59,7 +59,7 @@ function initSearchBox(){
 	});
 }
 
-function fetchSpotted() {
+function fetchSpotteds() {
 	var bounds = map.getBounds();
 	var southLat = bounds.getSouthWest().lat();
    	var southLng = bounds.getSouthWest().lng();
@@ -77,7 +77,7 @@ function fetchSpotted() {
 			maxLat : northLat,
 			minLong: northLng,
 			maxLong: southLng,
-			locationOnly: false
+			locationOnly: true
 		},
 		success: function(response) {
 			spotteds = response;
@@ -88,24 +88,45 @@ function fetchSpotted() {
 	});
 }
 
-function setMarkers(spotteds) {
-	var markers = [];
-	for(var i = 0; i < spotteds.length; i++) {
-	  	var latLng = new google.maps.LatLng(spotteds[i].location.coordinates[1], spotteds[i].location.coordinates[0]);
-	  	var infowindow = new google.maps.InfoWindow({
-	  		content : spotteds[i].message
-  		});
+function fetchSpotted(id, callback) {
+	$.ajax({
+		url: 'https://nbyapi.mo-bergeron.com/v1/spotted/'+id,
+		type: 'GET',
+		dataType: 'json',
+		headers: { "Authorization": "Basic " + btoa("guest:sjSHJLfHwUEbQB4gtHnzdJh1WfwRaVwWQZtilJvB1pZG8u1gFUFtgmGEUti2kLjONmf5fJqdpzvd26fLvdb0mNdtKib8SXpgCXjmYKblMUQAPDJzjgBLlUNAp7w2hmVOaUEquC037s3ZpEWxcLtIK1zdTdX9QY28fKNfClz1f0j9Vo8vMbvD562jiF8zgZ1i8hiI10AqI3vIxbSDN9RCjMEVU0La8cnDLmFXyAhWCOVbjTdujAcVJ1QFEcYkJGot4Kkugx0cKD2WB8zxkZtnRj4kYxWHGB8eb5E0dgTrC3w7"),
+					"Service-Provider": "Guest" 
+				},
+		success: function(response) {
+			callback(response);
+		},
+		error: function(xhr) {
+		}
+	});
+}
 
-  		var marker = new google.maps.Marker({
-  			position: latLng
+function setMarkers(spotteds) {
+
+	console.log(spotteds);
+	var markers = spotteds.map(function(spotted, i) {
+		var latLng = new google.maps.LatLng(spotted.location.coordinates[1], spotted.location.coordinates[0]);
+		
+        var marker = new google.maps.Marker({
+  			position: latLng,
+  			map : map
   		});
 
   		marker.addListener('click', function() {
-  			infowindow.open(map, marker);
+  			fetchSpotted(spotted._id ,function(output){
+  				var infowindow = new google.maps.InfoWindow({
+	  				content : "<h3>" + output.message + "</h3></br><img style=\"max-width: 100%; height: auto;\" src=\""+ output.pictureURL + "\">"
+  				});
+
+  				infowindow.open(map, marker);
+  			});
   		});
 
-  		markers.push(marker);
-	}
+  		return marker;
+    });
 
 	var markerCluster = new MarkerClusterer(map, markers,
     	{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
