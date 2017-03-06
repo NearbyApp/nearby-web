832f5f6fe2e5f467343d8a4d7c632228bf4856e4
David Lebrun <dave.lebr1@gmail.com>
2017-03-05 21:57:26 -0500

    Implement the Spiderfier without eventlistener

--- assets/js/maps2.js
+++ assets/js/maps2.js
@@ -12,7 +12,7 @@ window.onload = function() {
 
   spotteds = [];
 	markerCluster = new MarkerClusterer(map, [],
-    	{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    	{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m', maxZoom: 15});
 
 	initSearchBox();
 	map.addListener('idle', function() {
@@ -67,7 +67,7 @@ function fetchSpotteds() {
    	var northLat = bounds.getNorthEast().lat();
    	var northLng = bounds.getNorthEast().lng();
 	$.ajax({
-		url: 'https://nbyapi.mo-bergeron.com/v1/spotteds',
+		url: 'https://test.mo-bergeron.com/v1/spotteds',
 		type: 'GET',
 		dataType: 'json',
 		headers: { "Authorization": "Basic " + btoa("guest:sjSHJLfHwUEbQB4gtHnzdJh1WfwRaVwWQZtilJvB1pZG8u1gFUFtgmGEUti2kLjONmf5fJqdpzvd26fLvdb0mNdtKib8SXpgCXjmYKblMUQAPDJzjgBLlUNAp7w2hmVOaUEquC037s3ZpEWxcLtIK1zdTdX9QY28fKNfClz1f0j9Vo8vMbvD562jiF8zgZ1i8hiI10AqI3vIxbSDN9RCjMEVU0La8cnDLmFXyAhWCOVbjTdujAcVJ1QFEcYkJGot4Kkugx0cKD2WB8zxkZtnRj4kYxWHGB8eb5E0dgTrC3w7"),
@@ -92,7 +92,7 @@ function fetchSpotteds() {
 
 function fetchSpotted(id, callback) {
 	$.ajax({
-		url: 'https://nbyapi.mo-bergeron.com/v1/spotted/'+id,
+		url: 'https://test.mo-bergeron.com/v1/spotted/'+id,
 		type: 'GET',
 		dataType: 'json',
 		headers: { "Authorization": "Basic " + btoa("guest:sjSHJLfHwUEbQB4gtHnzdJh1WfwRaVwWQZtilJvB1pZG8u1gFUFtgmGEUti2kLjONmf5fJqdpzvd26fLvdb0mNdtKib8SXpgCXjmYKblMUQAPDJzjgBLlUNAp7w2hmVOaUEquC037s3ZpEWxcLtIK1zdTdX9QY28fKNfClz1f0j9Vo8vMbvD562jiF8zgZ1i8hiI10AqI3vIxbSDN9RCjMEVU0La8cnDLmFXyAhWCOVbjTdujAcVJ1QFEcYkJGot4Kkugx0cKD2WB8zxkZtnRj4kYxWHGB8eb5E0dgTrC3w7"),
@@ -110,14 +110,23 @@ function fetchSpotted(id, callback) {
 function setMarkers(spotteds) {
 
 	var markers = spotteds.map(function(spotted, i) {
-		var latLng = new google.maps.LatLng(spotted.location.coordinates[1], spotted.location.coordinates[0]);
 
-        var marker = new google.maps.Marker({
+      var loc = new gm.LatLng(spotted.location.coordinates[1], spotted.location.coordinates[0]);
+      var marker = new gm.Marker({
+        position: loc,
+        icon: 'images/marker.png',
+        map: map
+      });
+      oms.addMarker(marker);
+    //var latLng = new google.maps.LatLng(spotted.location.coordinates[1], spotted.location.coordinates[0]);
+
+    /*var marker = new google.maps.Marker({
   			position: latLng,
 				icon: 'images/marker.png',
   			map : map
   		});
-      
+
+
   		marker.addListener('click', function() {
   			fetchSpotted(spotted._id ,function(output) {
   				var windowContent = "";
@@ -142,11 +151,12 @@ function setMarkers(spotteds) {
   				infowindow.open(map, marker);
   			});
   		});
-
+      */
   		return marker;
     });
 
     markerCluster.addMarkers(markers);
+
 }
 
 function getNewSpotteds(response) {
